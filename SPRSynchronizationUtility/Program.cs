717072531e34d730b893078a4fda63aa94974ce2
Gagan Dhamija <gagan.bechtel@gmail.com>
2013-03-25 13:32:14 +0530

    Update Program.cs

--- SPRSynchronizationUtility/Program.cs
+++ SPRSynchronizationUtility/Program.cs
@@ -1,65 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.IO;
+using System.Windows.Forms;
 
 namespace Bechtel.iRING.SPRUtility
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
-            StreamWriter logFile=null;
-
-            try
-            {
-                if (!File.Exists("Log.txt"))
-                {
-                    logFile = new StreamWriter("Log.txt");
-                }
-                else
-                {
-                    logFile = File.AppendText("Log.txt");
-                }
-
-                logFile.WriteLine("Start Time:" + DateTime.Now);
-     
-                Console.WriteLine("SPR File Updater...");
-                SPRSynchronizationUtility syncUtility = new SPRSynchronizationUtility(logFile);
-                syncUtility.MDBSynchronization();
-                // syncUtility.PopulateSpool();
-
-                Console.WriteLine("File Update complete.");
-                logFile.WriteLine("Log file saved successfully!");
-                logFile.WriteLine("End Time:" + DateTime.Now);
-                logFile.Close();
-            }
-            catch (Exception ex)
-            {
-                logFile.WriteLine("Whoops! Please contact the developers with the following"
-                         + " information:\n\n" + ex.Message + ex.StackTrace,ex.InnerException.ToString(),
-                         "Fatal Error");
-                logFile.Close();
-            }
-        }
-
-        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            try
-            {
-                Exception ex = (Exception)e.ExceptionObject;
-
-                Console.WriteLine("Whoops! Please contact the developers with the following"
-                      + " information:\n\n" + ex.Message + ex.StackTrace,
-                      "Fatal Error");
-            }
-            finally
-            {
-                AppDomain.Unload(AppDomain.CurrentDomain);
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmSPRSynchronizationUtility());
+            Application.UseWaitCursor = false;
         }
     }
-   
 }
